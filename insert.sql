2c9d5b7ca6203acb0fcdb2b50d009928b4c51309
stoneshik <strebilly@mail.ru>
2023-09-23 19:14:42 +0300

    рефакторинг

--- insert.sql
+++ insert.sql
@@ -1,7 +1,19 @@
 -- Функция для генерации случайных байтовых значение типа bytea (нужно для имитации видеоконтента)
 CREATE OR REPLACE FUNCTION random_bytea(bytea_length integer)
     RETURNS bytea AS $body$
-SELECT decode(string_agg(lpad(to_hex(width_bucket(random(), 0, 1, 256)-1),2,'0') ,''), 'hex')
+SELECT decode(
+    string_agg(
+        lpad(
+            to_hex(
+                width_bucket(random(), 0, 1, 256) - 1
+            ),
+            2,
+            '0'
+        ),
+        ''
+    ),
+    'hex'
+)
 FROM generate_series(1, $1);
 $body$
     LANGUAGE 'sql'
@@ -18,9 +30,14 @@ VALUES (
     'Белый'
 );
 
-INSERT INTO videograms(vid_name, vid_date_of_creation, vid_content)
-VALUES ('Первое видео', default, random_bytea(1000)),
-       ('Второе видео', default, random_bytea(912)),
-       ('Третье видео', default, random_bytea(450));
+INSERT INTO videograms(vid_id, vid_name, vid_date_of_creation, vid_content)
+VALUES (0, 'Первое видео', default, random_bytea(1000)),
+       (1, 'Второе видео', default, random_bytea(912)),
+       (2, 'Третье видео', default, random_bytea(450));
 
-INSERT INTO
+INSERT INTO objects_in_videograms(obj_id, vid_id)
+VALUES (0, 0),
+       (0, 1),
+       (0, 2);
+
+INSERT INTO users()
